1c703de11e5ff580a9817749ce4d856771f5ef52
mneru002 <mihika.nerurkar@intel.com>
2025-04-28 13:37:59 -0700

    Fixing typo again

--- examples/create-parameters/main.tf
+++ examples/create-parameters/main.tf
@@ -1,6 +1,6 @@
 # Example of how to create the parameters only
 
-# Provision Intel Optimized AWS MSSQL server
+# Provision Intel Optimized AWS MSSQL server. 
 module "aws-mysql-parameter-group" {
   source = "intel/aws-mysql-parameter-group/intel"
 }
